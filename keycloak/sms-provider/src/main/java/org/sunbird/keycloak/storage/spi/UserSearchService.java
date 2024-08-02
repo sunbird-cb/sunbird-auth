5f117bd1b4c8c24e9ea2109fd52c4f45c61446ba
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2024-08-02 16:14:22 +0530

    changes for adding user_roles and orgid in the auth-token generation.

--- keycloak/sms-provider/src/main/java/org/sunbird/keycloak/storage/spi/UserSearchService.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/storage/spi/UserSearchService.java
@@ -30,7 +30,7 @@ public class UserSearchService {
     Map<String, Object> request = new HashMap<>();
     request.put("key",key.toLowerCase());
     request.put("value", value);
-    request.put("fields", Arrays.asList("email","firstName","lastName","id","phone","userName","countryCode","status"));
+    request.put("fields", Arrays.asList("email","firstName","lastName","id","phone","userName","countryCode","status","rootorgid","roles"));
     userRequest.put("request", request);
     String userLookupUrl = System.getenv("sunbird_user_service_base_url")+"/private/user/v1/lookup";
     Map<String, Object> resMap =
@@ -67,6 +67,15 @@ public class UserSearchService {
     user.setPhone((String) userMap.get(Constants.PHONE));
     user.setUsername((String) userMap.get("userName"));
     user.setCountryCode((String) userMap.get("countryCode"));
+    user.setOrg((String) userMap.get("rootOrgId"));
+    if ( null != userMap.get("roles") && ((List)userMap.get("roles")).size() > 0) {
+      user.setRoles((List<String>) userMap.get("roles"));
+    }
+    else{
+      List roles = new ArrayList();
+      roles.add("");
+      user.setRoles(roles);
+    }
     if ( null != userMap.get("status") && ((Integer)userMap.get("status")) == 0) {
       user.setEnabled(false);
     } else {
